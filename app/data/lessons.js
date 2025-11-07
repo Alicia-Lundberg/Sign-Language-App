c3a714e574a0d73f8da40e01d4a9e7e944ca366e
Alicia-Lundberg <alicia.lundberg00@gmail.com>
2025-11-07 11:21:21 +0100

    added level 3

--- app/data/lessons.js
+++ app/data/lessons.js
@@ -20,5 +20,18 @@ export const lessonsData = [
       { question: 'Vad är rätt här?', video: 'D', options: ['D','E','F', 'D'], correct: 0 },
       { question: 'Vad visas här?', video: 'E', options: ['G','E','F', 'D'], correct: 1 },
     ]
+  },
+  {
+    id: 3,
+    title: 'Level 3',
+    lessons: [
+      { question: 'Vad betyder A?', video: 'A', options: ['A','B','C', 'D'], correct: 0 },
+      { question: 'Vad betyder B?', video: 'B', options: ['A','B','C', 'D'], correct: 1 },
+      { question: 'Vad betyder C?', video: 'C', options: ['A','C','B', 'D'], correct: 1 },
+      { question: 'Vad är rätt här?', video: 'D', options: ['D','E','F', 'D'], correct: 0 },
+      { question: 'Vad visas här?', video: 'E', options: ['G','E','F', 'D'], correct: 1 },
+    ]
   }
+
+
 ]
